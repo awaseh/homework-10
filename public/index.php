0d65ddb5182119dc7ac6cb64cb81afa638a36c74
Abdul Waseh <abdulwaseh@Mac.home>
2024-11-21 00:13:17 -0500

    eveything of hw 10

--- public/index.php
+++ public/index.php
@@ -1,7 +1,9 @@
 <?php
 require_once "../app/models/Model.php";
 require_once "../app/models/User.php";
+require_once "../app/models/Post.php";
 require_once "../app/controllers/UserController.php";
+require_once "../app/controllers/PostController.php";
 
 //set our env variables
 $env = parse_ini_file('../.env');
@@ -11,23 +13,18 @@ define('DBUSER', $env['DBUSER']);
 define('DBPASS', $env['DBPASS']);
 
 use app\controllers\UserController;
+use app\controllers\PostController;
 
 //get uri without query strings
 $uri = strtok($_SERVER["REQUEST_URI"], '?');
 
 //get uri pieces
 $uriArray = explode("/", $uri);
-//0 = ""
-//1 = users
-//2 = 1
 
-
-//get all or a single user
+// User Routes (existing)
 if ($uriArray[1] === 'api' && $uriArray[2] === 'users' && $_SERVER['REQUEST_METHOD'] === 'GET') {
-    //only id
     $id = isset($uriArray[3]) ? intval($uriArray[3]) : null;
     $userController = new UserController();
-
     if ($id) {
         $userController->getUserByID($id);
     } else {
@@ -35,29 +32,52 @@ if ($uriArray[1] === 'api' && $uriArray[2] === 'users' && $_SERVER['REQUEST_METH
     }
 }
 
-//save user
 if ($uriArray[1] === 'api' && $uriArray[2] === 'users' && $_SERVER['REQUEST_METHOD'] === 'POST') {
     $userController = new UserController();
     $userController->saveUser();
 }
 
-//update user
 if ($uriArray[1] === 'api' && $uriArray[2] === 'users' && $_SERVER['REQUEST_METHOD'] === 'PUT') {
     $userController = new UserController();
     $id = isset($uriArray[3]) ? intval($uriArray[3]) : null;
     $userController->updateUser($id);
 }
 
-//delete user
 if ($uriArray[1] === 'api' && $uriArray[2] === 'users' && $_SERVER['REQUEST_METHOD'] === 'DELETE') {
     $userController = new UserController();
     $id = isset($uriArray[3]) ? intval($uriArray[3]) : null;
     $userController->deleteUser($id);
 }
 
-//views
+// Post Routes (new)
+if ($uriArray[1] === 'api' && $uriArray[2] === 'posts' && $_SERVER['REQUEST_METHOD'] === 'GET') {
+    $id = isset($uriArray[3]) ? intval($uriArray[3]) : null;
+    $postController = new PostController();
+    if ($id) {
+        $postController->getPostByID($id);
+    } else {
+        $postController->getAllPosts();
+    }
+}
 
+if ($uriArray[1] === 'api' && $uriArray[2] === 'posts' && $_SERVER['REQUEST_METHOD'] === 'POST') {
+    $postController = new PostController();
+    $postController->savePost();
+}
 
+if ($uriArray[1] === 'api' && $uriArray[2] === 'posts' && $_SERVER['REQUEST_METHOD'] === 'PUT') {
+    $postController = new PostController();
+    $id = isset($uriArray[3]) ? intval($uriArray[3]) : null;
+    $postController->updatePost($id);
+}
+
+if ($uriArray[1] === 'api' && $uriArray[2] === 'posts' && $_SERVER['REQUEST_METHOD'] === 'DELETE') {
+    $postController = new PostController();
+    $id = isset($uriArray[3]) ? intval($uriArray[3]) : null;
+    $postController->deletePost($id);
+}
+
+// User Views (existing)
 if ($uri === '/users-add' && $_SERVER['REQUEST_METHOD'] === 'GET') {
     $userController = new UserController();
     $userController->usersAddView();
@@ -73,6 +93,28 @@ if ($uriArray[1] === 'users-delete' && $_SERVER['REQUEST_METHOD'] === 'GET') {
     $userController->usersDeleteView();
 }
 
+// Post Views (new)
+if ($uri === '/posts' && $_SERVER['REQUEST_METHOD'] === 'GET') {
+    $postController = new PostController();
+    $postController->postsView();
+}
+
+if ($uri === '/posts-add' && $_SERVER['REQUEST_METHOD'] === 'GET') {
+    $postController = new PostController();
+    $postController->postsAddView();
+}
+
+if ($uriArray[1] === 'posts-update' && $_SERVER['REQUEST_METHOD'] === 'GET') {
+    $postController = new PostController();
+    $postController->postsUpdateView();
+}
+
+if ($uriArray[1] === 'posts-delete' && $_SERVER['REQUEST_METHOD'] === 'GET') {
+    $postController = new PostController();
+    $postController->postsDeleteView();
+}
+
+// Default route
 if ($uriArray[1] === '' && $_SERVER['REQUEST_METHOD'] === 'GET') {
     $userController = new UserController();
     $userController->usersView();
@@ -80,7 +122,5 @@ if ($uriArray[1] === '' && $_SERVER['REQUEST_METHOD'] === 'GET') {
 
 include '../public/assets/views/notFound.html';
 exit();
-
-?>
 
 
